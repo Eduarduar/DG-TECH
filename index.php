d9636d276b5208b484edb2f0879d6dfcfb65f397
Eduarduar <eduardoarcega1997@gmail.com>
2022-04-27 23:16:51 -0500

    :wq

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=0.5">
+    <link rel="shortcut icon" href="img/Logo-icon-trasparent.png" type="image/x-icon">
+    <link rel="stylesheet" href="css/header.css">
+    <title></title>
+</head>
+<body>
+
+    <!-- header -->
+
+    <?php include('secciones/header.php') ?>
+
+    <!-- header -->
+
+    <!-- main -->
+
+    <main>
+
+    </main>
+
+    <!-- main -->
+
+    <script src="js/despegable.js"></script>
+    
+</body>
+</html>
---
+++ secciones/header.php
@@ -0,0 +1,31 @@
+<header>
+
+        <div class="logo-div">
+
+            <img class="logo" src="img/Logo-icon-Trasparent.png" alt="Logo">
+
+        </div>
+        
+        <div class="titleFirts">
+
+            <h1 class="title">DGE-TECH</h1>
+
+        </div>
+
+        <nav>
+
+            <ul class="nav">
+                <li><a href="index.php">Inicio</a></li>
+                <li><a href="">Productos</a></li>
+                <li><a href="">¿Quienes somos?</a>
+                    <ul>
+                        <li><a href="">Servicios</a></li>
+                        <li><a href="">Contacto</a></li>
+                    </ul>
+
+                </li>
+            </ul>
+
+        </nav>
+
+    </header>
