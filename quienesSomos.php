e5b4982f1db5f734de9ae895dc735a556723ac28
Alfredo <elizaldiromero14@gmail.com>
2022-05-01 15:51:20 -0500

    Actualización de la pagina de Nosotros
    Agregue un css para la pagina y la información pero esta incompleta le falta el acomodo lo voy a continuar mañana o mas al rato

--- quienesSomos.php
+++ quienesSomos.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=0.5">
     <link rel="shortcut icon" href="img/Logo-icon-trasparent.png" type="image/x-icon">
     <link rel="stylesheet" href="css/header.css">
+    <link rel="stylesheet" href="css/sobreNosostro.css">
     <title>DGE-TECH</title>
 </head>
 <body>
@@ -17,10 +18,65 @@
     <!-- header -->
 
     <!-- main -->
+    <div class="prueba">
+        <img class="puerta" src="https://i.pinimg.com/564x/74/8f/af/748fafebbf96b64004f1169dd4b74b62.jpg" alt="">
+    </div>
+    
+    <article>
 
-    <main>
+    <section class="art1">
+        <h1>DGE-TECH</h1>
+        <p>
+        DD Tech surge para atender las crecientes necesidades mostradas por el mercado de consumidores de electronicos y computo</br> 
+        con la idea de satisfacerlas de una forma eficaz, ágil y con precios accesibles, además somos uno de los más grandes</br> 
+        vendedores en la plataforma Mercado Libre (distinguidos con el nombramiento de mercadolider platinum, categoría que se le</br> 
+        otorga sólo a vendedores miembros de la comunidad que gozan de una reputación impecable).</br></br>
 
-    </main>
+        Actualmente contamos con una oficina central y una sucursal ubicada en la zona centro de la ciudad de Guadalajara, Jalisco,</br> 
+        desde sus inicios nos propusimos ser una empresa que crease una conexión más directa entre sus clientes y personal, que los</br> 
+        guiará a realizar una buena compra.</br></br>
+
+        Más de 7 años brindando atención, servicio, calidad e innovación.</br>
+        </p>
+    </section>
+
+    <section class="art1">
+        <h1>Objetivo general</h1>
+        <p>
+        Nuestro objetivo general es satisfacer de manera eficaz las necesidades del mercado, buscando la mejora continua en los</br> 
+        servicios de la empresa.</br>
+        </p>
+    </section>
+
+    <section class="art1">
+        <h1>Misión</h1>
+        <p>
+        Ser el aliado estratégico de nuestros clientes, guiándolos a</br> 
+        una compra inteligente.</br>
+        </p>
+    </section>
+
+    <section class="art1">
+        <h1>Visión</h1>
+        <p>
+        Ser el mejor proveedor de tecnología y servicio online para un</br> 
+        mercado en constante cambio.</br>
+        </p>
+    </section>
+
+    <section class="art1">
+        <h1>Valores</h1>
+        <ol>
+            <li><b>Integridad:</b> Estamos expresamente comprometidos a que todas nuestras transacciones de negocios se realicen con integridad, honestidad y siguiendo normas éticas de conducta</li>
+            <li><b>Receptividad:</b> Como una organización, reconocemos la importancia de ser receptivos a las necesidades de todos aquellos con los cuales hacemos negocios, independiente de su ubicación.</li>
+            <li><b>Respeto:</b> A las ideas y diferencias individuales y culturales.</li>
+            <li><b>Trabajo en equipo:</b> DD Tech considera de gran valor el trabajo en equipo entre sus departamentos. Este espíritu debe prevalecer en toda actividad, incluyendo las relaciones con los clientes.</li>
+            <li><b>Servicio al cliente:</b> Servicio al cliente: DD Tech considera de vital importancia ofrecer excelencia en el servicio, proporcionado a nuestros clientes “lo que el cliente necesita, en donde lo necesita y cuando lo necesita”.</li>
+            <li><b>Responsabilidad:</b> Estamos comprometidos en ser un socio de negocios que siempre cumpla con lo prometido.</li>
+        </ol>
+    </section>
+
+    </article>
 
     <!-- main -->
     
